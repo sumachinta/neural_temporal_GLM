c334bba056f4d50cd18d2ab7612a980685cb077d
sumachinta <suma.bits@gmail.com>
2024-12-21 23:30:40 -0500

    Added test size variable to config

--- config/.ipynb_checkpoints/config-checkpoint.py
+++ config/.ipynb_checkpoints/config-checkpoint.py
@@ -2,15 +2,17 @@ from typing import List
 
 class Config:
     """Configuration class to store analysis parameters."""
-    def __init__(self, lags: int, n_shuffles: int, alpha_values: List[int]):
+    def __init__(self, lags: int, n_shuffles: int, alpha_values: List[int], test_size: float):
         self.lags = lags
         self.n_shuffles = n_shuffles
         self.alpha_values = alpha_values
+        self.test_size = test_size
 
 # Configuration setup
 config = Config(
-    lags=10,
-    n_shuffles=1000,
-    alpha_values=[700, 1000, 1200, 1500, 2000, 5000, 10000]
+    lags=10, # How much data to take around the current 't'
+    n_shuffles=1000, # Number of shuffles to get shuffled R2
+    alpha_values=[700, 1000, 1200, 1500, 2000, 5000, 10000], # Hyper paramtere tuning
+    test_size=0.2  # Fraction of data used for testing
 )
 
